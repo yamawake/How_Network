41bbff77b00f161d902e270eda2bda37ae437d8a
Yamawake Takahiro <yamawake@hotmail.com>
2018-12-16 02:19:48 +0900

    Makefile追加

--- param.h
+++ param.h
@@ -1,9 +1,17 @@
+#define	DEFAULT_MTU	(ETHERMTU)	// must inclue <net/ethernet.h>
+#define	DEFAULT_IP_TTL	(64)
+#define	DEFAULT_PING_SIZE	(64)
+
+
 typedef struct {
 	char	*device;
-	u_int8_t	mymac[6];
+	u_int8_t	mymac[6];	// must include <sys/types.h>
 	struct in_addr	myip;	// must include <netinet/in.h>
 	struct in_addr	vmask;	// must include <netinet/in.h>
-	int	IPTTL;
+	int	IpTTL;
 	int	MTU;
-	struct in_addr	gateway;	// must include <netine./in.h>
+	struct in_addr	gateway;	// must include <netinet/in.h>
 }PARAM;
+
+
+int SetDefaultParam();
