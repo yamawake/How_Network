6d74d6d5e10d5bdc41ef5f0daa3a4fe60cdb777c
Yamawake Takahiro <yamawake@hotmail.com>
2018-12-15 09:42:57 +0900

    param.h, param.c の追加

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <netinet/in.h>
+#include "param.h"
+
+PARAM	Param;
+
 
 int main(int argc, char *argv[])
 {
+	printf("IP-TTL=%d\n", Param.IPTTL);
+	printf("MTU=%d\n", Param.MTU);
+
+	return (0);
 }
---
+++ param.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sys/param.h>
+#include <sys/type.h>
+#include <sys/socket.h>
+
+#include "param.h"
+
+
+extern PARAM	Param;
