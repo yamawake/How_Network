0f907a021f8aaba862cafd9fefe4f95633f3de64
Yamawake Takahiro <yamawake@hotmail.com>
2018-12-18 04:11:14 +0900

    MyEth.ini追加 param.c編集

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include "param.h"
@@ -8,8 +9,22 @@ PARAM	Param;
 
 int main(int argc, char *argv[])
 {
+	int paramFlag;
+	paramFlag=0;
+
 	SetDefaultParam();
 
+	for (int i = 1; i < argc; i++){
+		if(ReadParam(argv[1])==-1){
+			exit(-1);	// must include <stdlib.h>
+		}
+	}
+	if(paramFlag==0){
+		if(ReadParam("./MyEth.ini")==-1){
+			exit(-1);	// must include <stdlib.h>
+		}
+	}
+	
 
 	printf("IP-TTL=%d\n", Param.IpTTL);
 	printf("MTU=%d\n", Param.MTU);
@@ -19,6 +34,7 @@ int main(int argc, char *argv[])
 	// show_ifreq(Param.device);
 	printf("+++++++++++++++++++++++++++++++++++++++\n");
 
+	//printf("vmac=%s\n", my_ether_ntoa_r(Param.vmac, buf1));
 
 	return (0);
 }
