42bf75207abbc0cedf0336354c3fe5a08a7ca415
Yamawake Takahiro <yamawake@hotmail.com>
2018-12-29 12:56:22 +0900

    ether.c ether.h の追加

---
+++ ether.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include "ether.h"
+#include "param.h"
+
+
+extern PARAM	Param;
+
+char *my_ether_ntoa_r(u_int8_t *hwaddr, char *buf){
+	
+	sprintf(buf, "%02x:%02x:%02x:%02x:%02x:%02x",
+			hwaddr[0], hwaddr[1], hwaddr[2], hwaddr[3], hwaddr[4], hwaddr[5]);
+
+	return(buf);
+}
+
+
+int my_ether_aton(char *str, u_int8_t *mac){
+
+	char	*ptr;
+	char	*saveptr=NULL;
+	char	*tmp=strdup(str);	// must include <string.h>
+	int	c;
+
+	for(c=0, ptr=strtok_r(tmp, ":", &saveptr); c < 6; c++ , ptr=strtok_r(NULL, ":", &saveptr)){
+		if(ptr==NULL){
+			free(tmp);	// must include <stdlib.h>
+			return(-1);
+		}
+		mac[c]=strtol(ptr, NULL, 16);
+	}
+	free(tmp);
+
+	return(0);
+}
---
+++ ether.h
@@ -0,0 +1,2 @@
+char *my_ether_ntoa_r(u_int8_t *hwaddr, char *buf);
+int my_ether_aton(char *str, u_int8_t *mac);
